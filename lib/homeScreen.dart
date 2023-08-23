82d6b8995a669012e28c51aa581fe64a68264303
FarisHollow <mirfaris79@gmail.com>
2023-08-24 01:07:53 +0600

    Firebase setup fixed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,17 +1,96 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  FirebaseFirestore firebaseFireStore = FirebaseFirestore.instance;
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Live score app'),
+      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('Football')
+              .doc('1_football_live')
+          // .collection('abs')
+          // .doc('dfdf')
+          // .collection('abs')
+          // .doc('dfdf')
+          // .collection('abs')
+          // .doc('dfdf')
+          // .collection('abs')
+          // .doc('dfdf')
+          // .collection('abs')
+          // .doc('dfdf')
+              .snapshots(),
+          builder:
+              (context, AsyncSnapshot<DocumentSnapshot<Object?>> snapshot) {
+            print(snapshot.data?.data());
+            if (snapshot.hasData) {
+              final score = snapshot.data!;
+              return Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    Text(
+                      score.get('match_name'),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              score.get('score_team_a').toString(),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              score.get('team_a'),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ],
+                        ),
+                        const Text('vs'),
+                        Column(
+                          children: [
+                            Text(
+                              score.get('score_team_b').toString(),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              score.get('team_b'),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('Football')
+              .doc('1_football_live')
+              .update({'match_name': "ARG VS IT", 'score_team_b': 7});
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'homeScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:football_live/homeScreen.dart';
+
+/// Let's an App that will show our basketball live score
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp;
-  runApp(const FootballLiveApp());
+  await Firebase.initializeApp();
+
+  runApp(const BasketBallLiveScoreApp());
 }
 
-class FootballLiveApp extends StatelessWidget {
-  const FootballLiveApp({super.key});
+class BasketBallLiveScoreApp extends StatelessWidget {
+  const BasketBallLiveScoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
+
