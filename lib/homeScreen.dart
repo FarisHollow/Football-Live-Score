7be10d85eb84861c67c2a9d9ac2098638b80128c
FarisHollow <mirfaris79@gmail.com>
2023-08-23 20:40:46 +0600

    Firebase setup for android manually

---
+++ lib/homeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore firebaseFireStore = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
